a4f1405f63347d2640f26bfca4f6a6aff4a17feb
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-08-23 12:21:45 +0300

    #1

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return str(self.title)
+    
+    
+class Waiter(models.Model):
