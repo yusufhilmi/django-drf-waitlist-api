a8c09e1319ebe59a047229e768cebdb112ff6169
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-08-24 23:44:49 +0300

    Viewset API

---
+++ projects/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from rest_framework import routers
+from projects import views
+
+router = routers.DefaultRouter()
+router.register(r'waiters', views.WaiterViewSet)
+
+waiter_create = views.WaiterViewSet.as_view({
+    'post': 'create',
+})
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,9 @@
-from rest_framework import generics
+from rest_framework import viewsets
 
 from projects.models import Waiter
 from projects.serializers import WaiterSerializer
 
 
-class WaiterCreate(generics.ListCreateAPIView):
+class WaiterViewSet(viewsets.ModelViewSet):
     queryset = Waiter.objects.all()
     serializer_class = WaiterSerializer
