f10759b643cfbd34b4a5281d60df4fb68ad30656
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-08-26 18:41:56 +0300

    Slug added to project

--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100, blank=False)
     description = models.TextField()
+    url = models.SlugField()
 
     def __str__(self):
         return str(self.title)
